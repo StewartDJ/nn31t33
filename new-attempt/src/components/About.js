3dd8ee665e3b06a767c237f2a21b30830d6c0e4f
DJ Stewart <deshastewart1992@gmail.com>
2020-08-04 23:57:02 -0700

    first commit

---
+++ new-attempt/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <div>
+    <div className='container'>
+      <h4 className="center">about</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, consectetur!</p>
+</div>
+    </div>
+  )
+}
+
+export default About
